a256bd1bf748ab1c95c9e6eb475906c0a9627880
Matheus Silveira <matheus@exemplo.com>
2023-08-08 19:37:02 -0300

    fix: limitação de casas decimais

--- main.js
+++ main.js
@@ -48,7 +48,7 @@ function atualizaTabela() {
 function atualizaMediaFinal (){
     const mediaFinal = calculaMediaFinal()
 
-    document.getElementById('media-final-valor').innerHTML = mediaFinal
+    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2)
     document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado
 }
 
@@ -61,3 +61,4 @@ function calculaMediaFinal() {
 
     return somaDasNotas / notas.length
 }
+
